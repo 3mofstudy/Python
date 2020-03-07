2290dede5e3f400ba5dff86f6f83b65564aa2343
3mofstudy <3mofstudy@gmail.com>
2020-03-07 21:26:21 +0800

    Create PYD105.py
    Creat PYD105

---
+++ TQC/TQC+ 程式語言第1類 基本程式設計/PYD105.py
@@ -0,0 +1,12 @@
+h = eval(input())
+w = eval(input())
+# TODO
+
+print('Height = {:.2f}\nWidth = {:.2f}\nPerimeter = {:.2f}\nArea = {:.2f}'.format(h,w,(h+w)*2,h*w))
+
+"""
+Height = {:2f}\n
+Width = {:2f}\n
+Perimeter = {:2f}\n
+Area = {:2f}
+"""
